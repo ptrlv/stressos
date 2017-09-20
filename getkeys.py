352ccc86cf6beeaf32cc34161aef00a40cd83551
Peter Love <p.love@lancaster.ac.uk>
2017-09-20 12:35:07 +0200

    python3 fix

--- getkeys.py
+++ getkeys.py
@@ -2,6 +2,7 @@
 import os
 import requests
 import urllib
+import urllib3
 
 """
 Retrieve object store keys from panda server
@@ -10,7 +11,7 @@ Retrieve object store keys from panda server
 def getSSLCertificate():
     """ Return the path to the SSL certificate """ 
 
-    if os.environ.has_key('X509_USER_PROXY'):
+    if 'X509_USER_PROXY' in os.environ:
         sslCertificate = os.environ['X509_USER_PROXY']
     else:
         sslCertificate  = '/tmp/x509up_u%s' % str(os.getuid())
@@ -18,10 +19,11 @@ def getSSLCertificate():
     return sslCertificate
 
 def getKeys(node):
+    urllib3.disable_warnings()
     r = requests.post('https://%s%s' % (host, path),
                                   verify=False,
                                   cert=(sslCert, sslKey),
-                                  data=urllib.urlencode(node),
+                                  data=urllib.parse.urlencode(node),
                                   timeout=120)
     if r and r.status_code == 200:
         print(r.text)
@@ -35,13 +37,13 @@ path = '/server/panda/getKeyPair'
 node={}
 node['privateKeyName'] = 'LANCS_ObjectStoreKey'
 node['publicKeyName'] = 'LANCS_ObjectStoreKey.pub'
-node['privateKeyName'] = 'BNL_ObjectStoreKey'
-node['publicKeyName'] = 'BNL_ObjectStoreKey.pub'
-node['privateKeyName'] = 'RAL_ObjectStoreKey'
-node['publicKeyName'] = 'RAL_ObjectStoreKey.pub'
-node['privateKeyName'] = 'CERN_ObjectStoreKey'
-node['publicKeyName'] = 'CERN_ObjectStoreKey.pub'
-node['privateKeyName'] = 'MWT2_ObjectStoreKey'
-node['publicKeyName'] = 'MWT2_ObjectStoreKey.pub'
+#node['privateKeyName'] = 'BNL_ObjectStoreKey'
+#node['publicKeyName'] = 'BNL_ObjectStoreKey.pub'
+#node['privateKeyName'] = 'RAL_ObjectStoreKey'
+#node['publicKeyName'] = 'RAL_ObjectStoreKey.pub'
+#node['privateKeyName'] = 'CERN_ObjectStoreKey'
+#node['publicKeyName'] = 'CERN_ObjectStoreKey.pub'
+#node['privateKeyName'] = 'MWT2_ObjectStoreKey'
+#node['publicKeyName'] = 'MWT2_ObjectStoreKey.pub'
 
 getKeys(node)
