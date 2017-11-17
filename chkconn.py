b58db88499e7f38b911c3d2268b665309797b750
Peter Love <p.love@lancaster.ac.uk>
2017-11-17 14:16:25 +0000

    list all buckets

--- chkconn.py
+++ chkconn.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 import argparse
+import random
+import string
 
 import boto
-import boto.s3.connection
+from boto.s3.connection import S3Connection
 
 """
 Check boto connection to s3
@@ -21,8 +23,7 @@ args = parser.parse_args()
 
 def get_connection(access_key, secret_key, host, port, is_secure):
     
-    #create the connection to the S3 server
-    conn = boto.connect_s3(
+    conn = S3Connection(
         aws_access_key_id = access_key,
         aws_secret_access_key = secret_key,
         host = host,
@@ -30,7 +31,6 @@ def get_connection(access_key, secret_key, host, port, is_secure):
         is_secure = is_secure,
         calling_format = boto.s3.connection.OrdinaryCallingFormat(),
         )
-    print('Make connection to remote host {}'.format(host))
     
     return conn
 
@@ -44,6 +44,8 @@ def main():
 
     print(conn)
     print(conn.get_path())
+    buckets = conn.get_all_buckets()
+    print(*buckets, sep='\n')
 
 if __name__ == '__main__':
     main()
